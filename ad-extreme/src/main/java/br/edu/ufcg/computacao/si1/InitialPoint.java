7a3a1bc03f848a1379fd043912cb50edb77e01d3
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2017-03-15 23:52:26 -0300

    indicador de branch adicionado

--- ad-extreme/src/main/java/br/edu/ufcg/computacao/si1/InitialPoint.java
+++ ad-extreme/src/main/java/br/edu/ufcg/computacao/si1/InitialPoint.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InitialPoint {
 
+    //Brito branch
 	public static void main(String[] args) {
 		SpringApplication.run(InitialPoint.class, args);
 	}
