e90cb5e930842cfc79b5b77dad88f3f74e47cba0
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2017-03-16 11:06:06 -0300

    Initial resource has been removed

--- ad-extreme/src/main/java/br/edu/ufcg/computacao/si1/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ufcg.computacao.si1.repository;
-
-import br.edu.ufcg.computacao.si1.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
-
-    Usuario findByEmail(String email);
-}
