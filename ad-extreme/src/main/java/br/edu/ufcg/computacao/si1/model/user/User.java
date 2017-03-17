7e466fee77e1dedd23b611e9f43ddfbca4734ba9
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2017-03-16 23:32:23 -0300

    some modifications

--- ad-extreme/src/main/java/br/edu/ufcg/computacao/si1/model/user/User.java
+++ ad-extreme/src/main/java/br/edu/ufcg/computacao/si1/model/user/User.java
@@ -23,6 +23,43 @@ public class User{
 
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public PersonType getPersonType() {
+        return personType;
+    }
+
+    public void setPersonType(PersonType personType) {
+        this.personType = personType;
+    }
 }
