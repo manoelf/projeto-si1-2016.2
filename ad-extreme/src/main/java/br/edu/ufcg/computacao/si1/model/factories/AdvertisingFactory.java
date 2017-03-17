d88ff555502f9a689f71a14578a8805128a66109
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2017-03-16 21:57:06 -0300

    creation of the advertising form

--- ad-extreme/src/main/java/br/edu/ufcg/computacao/si1/model/factories/AdvertisingFactory.java
+++ ad-extreme/src/main/java/br/edu/ufcg/computacao/si1/model/factories/AdvertisingFactory.java
@@ -7,6 +7,10 @@ import br.edu.ufcg.computacao.si1.model.advertising.AdvertisingType;
  * Created by matth on 16/03/2017.
  */
 public class AdvertisingFactory {
+
+    private AdvertisingFactory() {
+    }
+
     public static Advertising createNewAdvertising(Long ownerId, String title, Double price, String adType) {
         AdvertisingType advertisingType = AdvertisingType.getType(adType);
         return new Advertising(ownerId, title, price, advertisingType);
---
+++ ad-extreme/src/main/java/br/edu/ufcg/computacao/si1/model/forms/AdvertisingForm.java
@@ -0,0 +1,34 @@
+package br.edu.ufcg.computacao.si1.model.forms;
+
+/**
+ * Created by gersonsales on 16/03/17.
+ */
+public class AdvertisingForm {
+    private String title;
+    private Double price;
+    private String type; //TODO
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
