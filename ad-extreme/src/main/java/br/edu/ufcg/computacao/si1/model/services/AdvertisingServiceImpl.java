8ce5135e42b5b6c377978d2dd36e3be9f296152b
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2017-03-16 21:05:03 -0300

    changes that makes advertising improvement

--- ad-extreme/src/main/java/br/edu/ufcg/computacao/si1/model/services/AdvertisingServiceImpl.java
+++ ad-extreme/src/main/java/br/edu/ufcg/computacao/si1/model/services/AdvertisingServiceImpl.java
@@ -7,16 +7,19 @@ import br.edu.ufcg.computacao.si1.model.repositories.AdvertisingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by matth on 16/03/2017.
  */
 @Service
-public class AdvertisingServiceImpl implements AdvertisingService{
+public class AdvertisingServiceImpl implements AdvertisingService {
+
     @Autowired
     private AdvertisingRepository advertisingRepository;
-
 
     @Override
     public Advertising getById(Long id) {
@@ -24,28 +27,38 @@ public class AdvertisingServiceImpl implements AdvertisingService{
     }
 
     @Override
-    public Set<Advertising> getByOwnerId(Long ownerId) {
+    public List<Advertising> getByOwnerId(Long ownerId) {
         return this.advertisingRepository.getByOwnerID(ownerId);
     }
 
     @Override
-    public void create(String adType, Long ownerId) {
-        AdvertisingFactory.createNewAdvertising(adType, ownerId);
-
+    public void create(Long ownerId, String title, Double price, String adType) {
+        Advertising newAd = AdvertisingFactory.createNewAdvertising(ownerId, title, price, adType);
+        this.advertisingRepository.save(newAd);
     }
 
     @Override
     public void delete(Long id) {
+        this.advertisingRepository.delete(id);
+    }
 
+    private void save(Advertising advertising) {
+        this.advertisingRepository.save(advertising);
     }
 
     @Override
     public void update(Advertising advertising) {
-
+        this.advertisingRepository.delete(advertising.getId());
+        save(advertising);
     }
 
     @Override
-    public Set<Advertising> getAll() {
-        return null;
+    public List<Advertising> getAll() {
+        return this.advertisingRepository.findAll();
+    }
+
+    @Override
+    public List<Advertising> getByType(AdvertisingType advertisingType) {
+        return this.advertisingRepository.getByType(advertisingType);
     }
 }
