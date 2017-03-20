99734cf69182e5f6f097bf5bffce08b312ed3cd4
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2017-03-19 23:45:26 -0300

    Organizing classes js

---
+++ ad-extreme/src/main/resources/static/js/AdvertisingController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by matth on 19/03/2017.
+ */
+
+appADExtreme.controller ("AdvertisingRegisterCtrl", function($scope, $http){
+
+
+});
--- ad-extreme/src/main/resources/static/js/app.js
+++ ad-extreme/src/main/resources/static/js/app.js
@@ -4,33 +4,3 @@
 
 var appADExtreme = angular.module("appADExtreme", []);
 
-appADExtreme.controller ("userRegisterCtrl", function($scope, $http){
-
-    $scope.userList = [];
-
-        $http({method: "GET", url: "http://localhost:8080/registerUser"}).then(function (response) {
-
-            $scope.userList = response.data;
-
-        }, function (response) {
-
-            console.log(response.data);
-            console.log(response.status);
-
-        });
-
-
-    $scope.saveUser = function (user) {
-
-        $http({method: "POST", url: "http://localhost:8080/registerUser"}).then(function (response) {
-
-            $scope.listaDeTarefasSelecionada.tarefas.push(response.data);
-
-        }, function (response) {
-
-            console.log(response.data);
-            console.log(response.status);
-
-        });
-    }
-});
