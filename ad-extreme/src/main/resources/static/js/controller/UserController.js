9ff3edaf4ece758b7fd5a4dcd30c051068c564d2
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2017-03-21 13:42:24 -0300

    Division of tasks of Service and Controller

--- ad-extreme/src/main/resources/static/js/controller/UserController.js
+++ ad-extreme/src/main/resources/static/js/controller/UserController.js
@@ -6,29 +6,5 @@ appADExtreme.controller ("userRegisterCtrl", function($scope, $http){
 
     $scope.userList = [];
 
-    $http({method: "GET", url: "http://localhost:8080/registerUser"}).then(function (response) {
 
-        $scope.userList = response.data;
-
-    }, function (response) {
-
-        console.log(response.data);
-        console.log(response.status);
-
-    });
-
-
-    $scope.saveUser = function (user) {
-
-        $http({method: "POST", url: "http://localhost:8080/registerUser"}).then(function (response) {
-
-            $scope.listaDeTarefasSelecionada.tarefas.push(response.data);
-
-        }, function (response) {
-
-            console.log(response.data);
-            console.log(response.status);
-
-        });
-    }
 });
