cf5c0ecd369d278c4b6032c80e48125d48487eb2
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2017-03-22 19:45:17 -0300

    Let's codinggit add .!

--- ad-extreme/src/main/resources/static/advertising_service.js
+++ ad-extreme/src/main/resources/static/advertising_service.js
@@ -2,7 +2,7 @@
  * Created by gersonsales on 21/03/17.
  */
 
-App.factory('AdvertisingService', ['$http', '$q', function ($http, $q) {
+app.factory('AdvertisingService', ['$http', '$q', function ($http, $q) {
     return {
         fetchAllAdvertisements: function () {
             return $http.get('http://localhost:8080/').then(
