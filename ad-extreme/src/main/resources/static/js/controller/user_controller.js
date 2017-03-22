ab31f780f3f16759227f3ca49135e3dc4e80b872
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2017-03-22 10:00:04 -0300

    Ok

--- ad-extreme/src/main/resources/static/js/controller/user_controller.js
+++ ad-extreme/src/main/resources/static/js/controller/user_controller.js
@@ -2,9 +2,18 @@
  * Created by matth on 19/03/2017.
  */
 
-appADExtreme.controller ("userController", ["$scope", "userService",function($scope, $http){
+appADExtreme.controller ("userController", ["$scope", "userService",function($scope, userService){
 
     $scope.userList = [];
 
+
+    $scope.createUser = function(user){
+        userService.createUser(user)
+            .then(
+                function(errResponse){
+                    console.error('Error while creating User.');
+                }
+            );
+    };
 
 }]);
