c01bc316cbd31816ec710bf4e59e8e1eac85a9e2
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2017-03-22 23:14:13 -0300

    Missing configs

---
+++ ad-extreme/src/main/resources/static/js/controllers/user_controller.js
@@ -0,0 +1,10 @@
+/**
+ * Created by matth on 19/03/2017.
+ */
+
+appADExtreme.controller ("userRegisterCtrl", function($scope, $http){
+    $scope.app = "Ad Extreme";
+    $scope.userList = [];
+
+
+});
