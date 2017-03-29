ac14309f65e12c1382dc3dd64ae730c049694a2c
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2017-03-28 22:32:34 -0300

    oadextreme has been added

---
+++ oadextreme/target/classes/static/js/controller/UserController.js
@@ -0,0 +1,34 @@
+/**
+ * Created by matth on 19/03/2017.
+ */
+
+appADExtreme.controller ("userRegisterCtrl", function($scope, $http){
+
+    $scope.userList = [];
+
+    $http({method: "GET", url: "http://localhost:8080/registerUser"}).then(function (response) {
+
+        $scope.userList = response.data;
+
+    }, function (response) {
+
+        console.log(response.data);
+        console.log(response.status);
+
+    });
+
+
+    $scope.saveUser = function (user) {
+
+        $http({method: "POST", url: "http://localhost:8080/registerUser"}).then(function (response) {
+
+            $scope.listaDeTarefasSelecionada.tarefas.push(response.data);
+
+        }, function (response) {
+
+            console.log(response.data);
+            console.log(response.status);
+
+        });
+    }
+});
